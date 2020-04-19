39d9033354241413946a7e42b25e309f772a5e42
to.eno <pekero.mochi@gmail.com>
2020-04-19 19:36:35 +0900

    Add Employee show

--- src/controllers/employees/EmployeesShowServlet.java
+++ src/controllers/employees/EmployeesShowServlet.java
@@ -2,12 +2,16 @@ package controllers.employees;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+import utils.DBUtil;
 /**
  * Servlet implementation class EmployeesShowServlet
  */
@@ -27,8 +31,16 @@ public class EmployeesShowServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        EntityManager em = DBUtil.createEntityManager();
+
+        Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("employee", e);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/show.jsp");
+        rd.forward(request, response);
     }
 
 
